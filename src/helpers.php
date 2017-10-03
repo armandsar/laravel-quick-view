13d0a02aed9c0c46ca91b0e8d0c267a11777a340
armandsar <armands.leinieks@gmail.com>
2017-10-03 21:14:18 +0300

    init

---
+++ src/helpers.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Contracts\View\Factory as ViewFactory;
+
+
+if ( ! function_exists('quick')) {
+
+    function quick($data = [], $mergeData = [])
+    {
+        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2)[1];
+
+        $class = $trace['class'];
+        $method = $trace['function'];
+
+        $class = str_replace("App\\Http\\Controllers\\", '', $class);
+        $class = preg_replace('/Controller$/', '', $class);
+        $class = snake_case($class);
+        $class = str_replace('\\_', '.', $class);
+
+        $view = $class . '.' . $method;
+
+        $factory = app(ViewFactory::class);
+
+        return $factory->make($view, $data, $mergeData);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Armandsar\QuickView\Tests {
+
+    use App\Http\Controllers\PostsController;
+    use App\Http\Controllers\Admin\SpecialPostsController;
+    use Illuminate\Contracts\View\Factory as ViewFactory;
+    use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+    class TestCase extends OrchestraTestCase
+    {
+        public function testHelperFunction()
+        {
+            $viewFactory = \Mockery::mock(ViewFactory::class);
+
+            app()->instance(ViewFactory::class, $viewFactory);
+
+            $viewFactory->shouldReceive('make')
+                ->with('admin.special_posts.index', ['a' => 1], ['b' => 2])
+                ->once();
+
+            $viewFactory->shouldReceive('make')
+                ->with('posts.create', [], [])
+                ->once();
+
+            (new SpecialPostsController)->index();
+            (new PostsController())->create();
+        }
+
+    }
+
+}
+
+namespace App\Http\Controllers {
+
+    class PostsController
+    {
+        public function create()
+        {
+            return quick();
+        }
+    }
+}
+
+namespace App\Http\Controllers\Admin {
+
+    class SpecialPostsController
+    {
+        public function index()
+        {
+            return quick(['a' => 1], ['b' => 2]);
+        }
+    }
+
+}
